d9673e45af1fbdb7883fc96650b5cb3abdd7a892
rubayet <rubayetislam16@gmail.com>
2025-04-19 23:55:46 +0600

    welcome

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json(['message' => 'API is running on this website']);
+    return view("welcome");
 });
